4db11759ebdccd34f89b35120f881614ccc0be9b
w-userc <127527378+w-userc@users.noreply.github.com>
2023-11-10 17:37:20 +0000

    test liedson

--- app/src/main/java/com/example/emwaver10/MainActivity.java
+++ app/src/main/java/com/example/emwaver10/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
+        // Passing each menu ID as a set of Ids because each liedson
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
