2bb3bd391cf13bd9e967b8b9ce71b69b245cc8d1
luispl <maarnotto@gmail.com>
2023-12-14 17:37:52 +0000

    added new script functionality, save files, load between 3 scripts, execute scripts. However, major bottleneck in arquitecture was discovered. We cannot broadcast intents when we switch fragments, from fragments. Only the service can do so, because it keeps running. This means major rethinking is needed regarding the way we pass information between fragments in our app.

--- app/src/main/java/com/example/emwaver10/ui/scripts/ScriptsFragment.java
+++ app/src/main/java/com/example/emwaver10/ui/scripts/ScriptsFragment.java
@@ -14,6 +14,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.example.emwaver10.jsobjects.CC1101;
 import com.example.emwaver10.CommandSender;
@@ -21,6 +24,15 @@ import com.example.emwaver10.Constants;
 import com.example.emwaver10.jsobjects.Serial;
 import com.example.emwaver10.jsobjects.Console;
 import com.example.emwaver10.databinding.FragmentScriptsBinding;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 public class ScriptsFragment extends Fragment implements CommandSender {
 
@@ -46,6 +58,30 @@ public class ScriptsFragment extends Fragment implements CommandSender {
         serial = new Serial(getContext(), this);
 
         console = new Console(getContext());
+
+        initializeScripts();
+
+
+        // Populate spinner with filenames
+        String[] fileNames = getJavaScriptFileNames();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, fileNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.spinnerFiles.setAdapter(adapter);
+        // Listener for spinner selection
+        binding.spinnerFiles.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                loadFileContent(fileNames[position]);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        // Button listeners for save and rename
+        binding.buttonSave.setOnClickListener(v -> saveFile());
+        binding.buttonRename.setOnClickListener(v -> renameFile());
+
 
         // Set up your button click listeners
         binding.executeScriptButton.setOnClickListener(new View.OnClickListener() {
@@ -147,5 +183,108 @@ public class ScriptsFragment extends Fragment implements CommandSender {
         }
         // Retrieve the response
         return scriptsViewModel.getAndClearResponse(expectedResponseSize);
+    }
+
+    private void loadFileContent(String fileName) {
+        File file = new File(getContext().getFilesDir(), fileName);
+        if (file.exists()) {
+            try {
+                FileInputStream fis = new FileInputStream(file);
+                byte[] buffer = new byte[(int) file.length()];
+                fis.read(buffer);
+                fis.close();
+                String content = new String(buffer, "UTF-8");
+                binding.jsCodeInput.setText(content);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                showToastOnUiThread("Error loading file");
+            }
+        }
+    }
+
+    private void saveFile() {
+        String selectedFile = binding.spinnerFiles.getSelectedItem().toString();
+        String fileContent = binding.jsCodeInput.getText().toString();
+
+        try {
+            FileOutputStream fos = getContext().openFileOutput(selectedFile, getContext().MODE_PRIVATE);
+            fos.write(fileContent.getBytes());
+            fos.close();
+            showToastOnUiThread("File saved successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+            showToastOnUiThread("Error saving file");
+        }
+    }
+
+    private void renameFile() {
+        String oldFileName = binding.spinnerFiles.getSelectedItem().toString();
+        String newFileName = binding.editTextNewFileName.getText().toString();
+
+        File oldFile = new File(getContext().getFilesDir(), oldFileName);
+        File newFile = new File(getContext().getFilesDir(), newFileName);
+
+        if (oldFile.renameTo(newFile)) {
+            showToastOnUiThread("File renamed successfully");
+            // Update spinner and any other relevant UI components
+            // Refresh file names in the spinner
+        } else {
+            showToastOnUiThread("Error renaming file");
+        }
+    }
+
+
+    private String[] getJavaScriptFileNames() {
+        File dir = getContext().getFilesDir();
+        FilenameFilter filter = (dir1, name) -> name.endsWith(".js");
+        File[] files = dir.listFiles(filter);
+
+        if (files != null) {
+            String[] fileNames = new String[files.length];
+            for (int i = 0; i < files.length; i++) {
+                fileNames[i] = files[i].getName();
+            }
+            return fileNames;
+        } else {
+            return new String[0]; // Return an empty array if no files are found
+        }
+    }
+
+
+
+    private void initializeScripts() {
+        String[] assetFileNames = new String[]{"script1.js", "script2.js", "script3.js"};
+        for (String fileName : assetFileNames) {
+            File file = new File(getContext().getFilesDir(), fileName);
+            if (!file.exists()) {
+                copyFileFromAssets(fileName);
+            }
+        }
+    }
+
+
+    private void copyFileFromAssets(String fileName) {
+        try {
+            InputStream is = getContext().getAssets().open(fileName);
+            FileOutputStream fos = new FileOutputStream(new File(getContext().getFilesDir(), fileName));
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, read);
+            }
+            fos.flush();
+            fos.close();
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void showToastOnUiThread(final String message) {
+        if (isAdded()) { // Check if Fragment is currently added to its activity
+            getActivity().runOnUiThread(() ->
+                    Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show());
+        }
     }
 }
